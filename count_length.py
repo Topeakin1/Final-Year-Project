04e5db19fce5581a4bb5c87d4f84367da33822dd
Topeakin1 <temitope.akinwale@ucdconnect.ie>
2020-02-25 19:19:09 +0000

    count_length.py gives the average length of the sentences in the reviews

--- count_length.py
+++ count_length.py
@@ -14,21 +14,30 @@ TRAIN_TEST_SPLIT = int(SAMPLE_SIZE * 0.5)
 feature_data = []
 wordcounts = []
 i = 0
+num_sentences = 0
+total_sentence_length = 0
 # stopwords = nltk.corpus.stopwords.words('english')
-with open('preprocessed_reviews.json') as data_file:
+with open('balanced_reviews.json') as data_file:
     for line in data_file:
         # load review text from JSON
         data_item = json.loads(line)
-        text = data_item['text']
+        text = data_item['review_text']
         sentences = text.split('.')
+        num_sentences += len(sentences)
+        i += 1
+        print(i)
         for sentence in sentences:
             words = sentence.split(' ')
-            wordcounts.append(len(words))
-            average_wordcount = sum(wordcounts)/len(wordcounts)
+            # wordcounts.append(len(words))
+            total_sentence_length += len(words)
+            # average_wordcount = sum(wordcounts)/len(wordcounts)
             #average_sentence_length = (average_wordcount / 150924)
             #print(average_sentence_length)
-            a = sum(wordcounts)/ 150924
-            print(a)
+            # a = sum(wordcounts)/ 150924
+            # print(a)
+
+average_sentence_length = total_sentence_length / num_sentences
+print(f"Average sentence length: {average_sentence_length}")
             
         
 
